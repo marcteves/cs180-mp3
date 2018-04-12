51c3c7085387e54096203205b99f11e852a6c74c
Marc Teves <gmarc.teves@gmail.com>
2018-04-13 02:19:50 +0800

    test.sh now generates both test and train lists

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 # Builds a list of emails that form the training set.
 # Then concatenates them all into one big file
+# this also builds the test set list
 find text/ -type f | shuf -n 45251 > training_list
+find text/ -type f > test_set_list
+diff --new-line-format="" --unchanged-line-format="" <(sort test_set_list) <(sort training_list) > test_set_list
+
 echo "hello" > training_concat_text
 cat training_list | xargs cat >> training_concat_text
