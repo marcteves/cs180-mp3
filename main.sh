360f070c1925d6ed40f1df88a3e238b0b4550d65
Marc Teves <gmarc.teves@gmail.com>
2018-04-13 15:11:49 +0800

    fixed race conditions with main.sh

--- main.sh
+++ main.sh
@@ -1,19 +1,20 @@
-TRAINING_TEXT=training_concat_text
-TRAINING_LIST=training_list
-TEST_LIST=test_set_list
-DICT_FILE=dictionary
-WORD_COUNT=35000
-NUM_TRAIN=45251
-DATA_PATH=trec07p/data/
-TEXT_PATH=text/
+source config.sh
 # strip the text parts from the emails
 find "$DATA_PATH" -type f | xargs ./strip_mail.py
 # Build a list of emails that form the training set.
 # Then concatenate them all into one big file.
-# this also builds the test set list
 find "$TEXT_PATH" -type f | shuf -n "$NUM_TRAIN" > "$TRAINING_LIST"
+# sort training list according to version sort
+cat "$TRAINING_LIST" | sort -V > "$TRAINING_LIST".$$
+mv "$TRAINING_LIST".$$ "$TRAINING_LIST"
+# build the test set list
 find "$TEXT_PATH" -type f > "$TEST_LIST"
-diff --new-line-format="" --unchanged-line-format="" <(sort test_set_list) <(sort "$TRAINING_LIST") > "$TEST_LIST"
+# take every line in file list that isn't in training list
+diff --new-line-format="%L" --unchanged-line-format="" <(sort "$TRAINING_LIST") <(sort "$TEST_LIST") > "$TEST_LIST".$$
+mv "$TEST_LIST".$$ "$TEST_LIST"
+# sort test list according to version sort
+cat "$TEST_LIST" | sort -V > "$TEST_LIST".$$
+mv "$TEST_LIST".$$ "$TEST_LIST"
 
 echo "hello" > "$TRAINING_TEXT"
 cat "$TRAINING_LIST" | xargs cat >> "$TRAINING_TEXT"
